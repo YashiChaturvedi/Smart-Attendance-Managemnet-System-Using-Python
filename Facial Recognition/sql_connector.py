4312c75e77d1e381ec4b011def9e38ce20b2e794
Yashi Chaturvedi <56186000+YashiChaturvedi@users.noreply.github.com>
2020-12-27 10:52:35 +0530

    Add files via upload

---
+++ Facial Recognition/sql_connector.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import time
+import datetime
+
+
+ts = time.time()
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="facial_recognition"
+    )
+mycursor = mydb.cursor()
+
+try:
+    mycursor.execute("""INSERT into user_name (time,name) values(%s,%s)""",(timestamp,id))
+    mycursor.commit()
+
+except:
+    mydb.rollback()
+
+mycursor.close()
+
+    
